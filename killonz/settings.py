70890e3292773a62b1d8acfa2556516a3cb5d692
Wafik Hamadou <aymenhamadou2000@gmail.com>
2026-03-07 13:54:48 +0100

    simple dimple

--- killonz/settings.py
+++ killonz/settings.py
@@ -75,6 +75,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_AGE = 86400 * 7  # 7 days
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
-CSRF_TRUSTED_ORIGINS = [
-    'https://your-app-name.up.railway.app',
-]
+CSRF_TRUSTED_ORIGINS = ['https://web-production-23e77.up.railway.app']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
