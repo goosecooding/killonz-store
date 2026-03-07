56ede3c88f3b3f6581a85de28fe25571d2265544
Wafik Hamadou <aymenhamadou2000@gmail.com>
2026-03-07 13:30:50 +0100

    final

--- killonz/settings.py
+++ killonz/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-killonz-store-change-this-in-production-2024'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
