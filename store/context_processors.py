8f75b965b54c132b9abd01eb8e78774d7ec88335
Wafik Hamadou <aymenhamadou2000@gmail.com>
2026-03-14 12:03:48 +0100

    Install project dependencies and set up the Python virtual environment.

--- store/context_processors.py
+++ store/context_processors.py
@@ -1,3 +1,8 @@
 def cart_count(request):
     cart = request.session.get('cart', {})
     return {'cart_count': sum(cart.values())}
+
+from django.utils.translation import get_language
+
+def language_context(request):
+    return {'current_lang': get_language()}
